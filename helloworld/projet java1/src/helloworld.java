e007e65c39ab074480987507f65c1c38925b2b54
Belaid IMAZATENE <bim.htc21@gmail.com>
2021-08-23 18:03:07 +0200

    add git ignore file + 2 simple java projects

---
+++ helloworld/projet java1/src/helloworld.java
@@ -0,0 +1,10 @@
+
+public class helloworld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.print("Hello, World\n");
+        System.out.print("My name is");
+        System.out.print("\nToTo belaid");
+        System.out.println();
+    }
+}
